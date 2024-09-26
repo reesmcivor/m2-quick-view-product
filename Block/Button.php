701777247b652070a195afcdcbdcdae030da2e88
reesmcivor <reesmcivor@hotmail.com>
2024-09-26 10:34:24 +0100

    fix: link to product rather than popup

--- Block/Button.php
+++ Block/Button.php
@@ -6,8 +6,27 @@ use Magento\Framework\View\Element\Template;
 
 class Button extends Template
 {
+    public mixed $product;
     public string $sku = "";
     public string $buttonText = "";
+    private $productRepository;
+
+    public function __construct(
+        Template\Context $context,
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+        array $data = [],
+    )
+    {
+        $this->productRepository = $productRepository;
+        parent::__construct($context, $data);
+    }
+
+    public function getProductUrl()
+    {
+        // Get product by SKU
+        $this->product = $this->productRepository->get($this->sku);
+        return $this->product->getProductUrl();
+    }
 
     public function setSku( $sku = '' )
     {
